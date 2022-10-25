5b4e2bc7f99f6e20baaa6b8bc9e6c4f3090fb190
Robert Ochoa <ochoa.robert1@gmail.com>
2022-10-24 22:14:31 -0400

    :children_crossing: Metabox + Responsive - Seccion Mayoristas
    Nuevo Responsive y Metabox de PDF para seccion de Mayoristas

--- includes/custom-metaboxes-mayoristas.php
+++ includes/custom-metaboxes-mayoristas.php
@@ -1,22 +1,49 @@
-<?php 
+<?php
+
 /* MAYORISTAS SLIDER */
-$cmb_mayoristas_slider = new_cmb2_box( array(
+$cmb_mayoristas_slider = new_cmb2_box(array(
     'id'            => $prefix . 'mayoristas_slider',
-    'title'         => esc_html__( 'beneficios: Slider Revolution', 'gespetfood' ),
+    'title'         => esc_html__('beneficios: Slider Revolution', 'gespetfood'),
     'object_types'  => array( 'page' ), // Post type
     'show_on'      => array( 'key' => 'page-template', 'value' => 'templates/templates-mayoristas.php' ),
     'context'    => 'normal',
     'priority'   => 'high',
     'show_names' => true, // Show field names on the left
     'closed'     => false, // true to keep the metabox closed by default
-) );
+));
 
-$cmb_mayoristas_slider->add_field( array(
+$cmb_mayoristas_slider->add_field(array(
     'id'         => $prefix . 'slider_rev',
-    'name'       => esc_html__( 'Slider Revolution', 'gespetfood' ),
-    'desc'       => esc_html__( 'Seleccione el Slider a usar en el beneficios', 'gespetfood' ),
+    'name'       => esc_html__('Slider Revolution', 'gespetfood'),
+    'desc'       => esc_html__('Seleccione el Slider a usar en el beneficios', 'gespetfood'),
     'type'             => 'select',
     'show_option_none' => true,
     'default'          => '',
     'options'          => $array_sliders
-) );
+));
+
+/* MAYORISTAS BROCHURE */
+$cmb_mayoristas_link = new_cmb2_box(array(
+    'id'            => $prefix . 'mayoristas_link',
+    'title'         => esc_html__('Informacion de Archivo', 'gespetfood'),
+    'object_types'  => array( 'page' ), // Post type
+    'show_on'      => array( 'key' => 'page-template', 'value' => 'templates/templates-mayoristas.php' ),
+    'context'    => 'normal',
+    'priority'   => 'high',
+    'show_names' => true, // Show field names on the left
+    'closed'     => false, // true to keep the metabox closed by default
+));
+
+$cmb_mayoristas_link->add_field(array(
+    'id'      => $prefix . 'mayoristas_link_url',
+    'name'      => esc_html__('PDF descarga', 'gespetfood'),
+    'desc'      => esc_html__('Ingrese una PDF apropiado para la sección', 'gespetfood'),
+    'type'    => 'file',
+    'options' => array(
+        'url' => false
+    ),
+    'text'    => array(
+        'add_upload_file_text' => esc_html__('Cargar PDF de Sección', 'gespetfood'),
+    ),
+    'preview_size' => 'medium'
+));
--- templates/templates-mayoristas.php
+++ templates/templates-mayoristas.php
@@ -17,22 +17,17 @@
         </section>
         <?php $bg_banner_id = get_post_meta(get_the_ID(), 'gpf_gespetfood_about_img_id', true); ?>
         <?php // $bg_banner = wp_get_attachment_image_src($bg_banner_id, 'full', false);?>
-        <?php $bg_banner = get_the_post_thumbnail_url(get_the_ID() , 'full'); ?>
+        <?php $bg_banner = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
         <section class="wholesale col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="background: #191919 url(<?php echo $bg_banner;?>);" data-aos="fade">
-        <img src="<?php echo $bg_banner;?>" alt="">
             <div class="container container-wholesale">
                 <div class="row no-gutters align-items-center">
-                    <div class="wholesale-info col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="wholesale-info col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12">
                         <?php the_content(); ?>
                     </div>
                     <div class="w-100"></div>
                 </div>
             </div>
         </section>
-        <div class="wholesale-catalog-container col-12">
-            <h2><?php _e('¿Quieres más información?', 'gespetfood'); ?></h2>
-            <a href="" class="btn btn-md btn-wholesale"><?php _e('Descarga el catálogo', 'gespetfood'); ?></a>
-        </div>
         <section class="wholesale-embed-form col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" data-aos="fade">
             <div class="container">
                 <div class="row justify-content-center">
@@ -44,6 +39,10 @@
                     </div>
                 </div>
             </div>
+        </section>
+        <section class="wholesale-catalog-container col-12">
+            <h2><?php _e('¿Quieres más información?', 'gespetfood'); ?></h2>
+            <a href="<?php echo get_post_meta(get_the_ID(), 'gpf_mayoristas_link_url', true); ?>" target="_blank" class="btn btn-md btn-wholesale"><?php _e('Descarga el catálogo', 'gespetfood'); ?></a>
         </section>
     </div>
 </main>
